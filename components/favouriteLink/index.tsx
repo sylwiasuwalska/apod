7658f7de0251209065c4e09c36ed92154bc69ac0
sylwia.suwalska <sylwia.suwalska@wundermanthompson.com>
2022-02-13 14:47:54 +0100

    Add Cypress to project

--- components/favouriteLink/index.tsx
+++ components/favouriteLink/index.tsx
@@ -9,7 +9,7 @@ function FavouriteLink() {
     return (
         <Tooltip title={'See your favourites'} placement="left-start" arrow>
             <Box sx={{ position: 'absolute', top: mobile ? 0 : '20px', right: mobile ? 0 : '20px' }}>
-                <Link href="/favourites">
+                <Link href="/favourites" passHref>
                     <IconButton sx={{ color: 'primary.header' }} aria-label={`Your favourites pictures`} size="large">
                         <FavoriteBorderIcon fontSize={mobile ? 'medium' : 'large'} />
                     </IconButton>
